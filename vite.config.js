4bb9971e273e11d404a66ff7624f35d6ab8bffe9
DDurant94 <dannyjdurant@gmail.com>
2024-11-11 02:21:03 -0600

    port fix

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 10000
+  },
+  preview: {
+    port: 10000
+  },
   define: {
     'process.env.NODE_ENV': '"production"'
   }
 });
-
