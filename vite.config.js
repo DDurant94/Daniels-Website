90b2c7bab503e7e0c76dd08fd1e5ec904307fe22
DDurant94 <dannyjdurant@gmail.com>
2026-03-09 11:18:58 -0500

    config test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/Daniels-Website/',
   plugins: [react()],
   server: {
     port: process.env.PORT || 10000,
